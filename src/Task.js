deef225573f86e76ff49aa3a03c0afd00a09aeb9
Siti Nur Fadilah Syam <92351188+Nurfadillah12@users.noreply.github.com>
2022-12-17 16:46:31 +0700

    first commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL"; // TODO: replace this
+const studentName = "Siti Nur Fadilah Syam"; // TODO: replace this
+const studentId = "FE2601826"; // TODO: replace this
+
+export { url, studentName, studentId };
